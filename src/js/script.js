79edc9a801c7e1025098b0fa3bf96ab05f54cfaf
Yevhenii <evgengorobets5@gmail.com>
2022-08-01 16:09:03 +0400

    some page

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    function openModal() {
+        const modal = document.querySelector('.promo_btn');
+        const box = document.querySelector('.promo');
+
+
+        modal.addEventListener('click', () => {
+            // const form = document.createElement('div.form');
+            // form.style.cssText = `
+            // position: absolute;
+            // z-index: 2;
+            // width: 250px;
+            // height: 250px;
+            // border: 2px solid black;
+            // background: yellow;
+            // `;
+            // form.prepend(box);
+
+        });
+    }
+    openModal();
+});
